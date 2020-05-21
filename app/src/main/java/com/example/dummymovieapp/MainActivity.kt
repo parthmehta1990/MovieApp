1c9e56d81b83b5163e1c8525becc7b20f80153f4
Parth Mehta <parth@id8labs.com>
2020-05-21 10:17:24 +0530

    Initial Project Setup + Integrated Page indicator to Banner(View Pager)

--- app/src/main/java/com/example/dummymovieapp/MainActivity.kt
+++ app/src/main/java/com/example/dummymovieapp/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.dummymovieapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var movieSliderData: ArrayList<Slide>
     lateinit var slidePager:ViewPager
-
+    lateinit var indicator:TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +38,30 @@ class MainActivity : AppCompatActivity() {
 
     fun createSlideBanner(){
         slidePager=findViewById(R.id.slider_pager)
+        indicator=findViewById(R.id.indicator)
 
         var sliderPageAdapter=SliderPageAdapter(this,movieSliderData)
         slidePager.adapter=sliderPageAdapter
+
+        var timer= Timer()
+        timer.scheduleAtFixedRate(SliderTimer(),4000,6000)
+
+        indicator.setupWithViewPager(slidePager,true)
+    }
+
+    internal inner class SliderTimer : TimerTask() {
+
+
+       override fun run() {
+
+            this@MainActivity.runOnUiThread(Runnable {
+                if (slidePager.getCurrentItem() < movieSliderData.size - 1) {
+                    slidePager.setCurrentItem(slidePager.getCurrentItem() + 1)
+                } else
+                    slidePager.setCurrentItem(0)
+            })
+
+
+        }
     }
 }
